3a33eedbe9e77065116400cdad56fc86477da19d
lapointek <lapointek.dev@gmail.com>
2025-10-02 20:01:27 -0400

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Description
+
+This is a web application where you fetch weather data using an API from https://openweathermap.org/api that displays weather information such as the temperature, wind speed, condition, and humidity.
+
+## Using the Weather Application
+
+### Run Application
+
+```
+npm start
+```
+
+### Local Server
+
+[http://localhost:3000](http://localhost:3000)
+
+## Weather Application Images
+
+![alt text](https://github.com/lapointek/weather-fetcher/blob/main/images/weather_app.png?raw=true)
+![alt text](https://github.com/lapointek/weather-fetcher/blob/main/images/searched_city.png?raw=true)
---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import "./App.css";
+
+const api = {
+  key:
+  base:
+};
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState("");
+
+  const searchPressed = async () => {
+    try {
+      const url = `${api.base}weather?q=${search}&units=metric&appid=${api.key}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      if (!response.ok) {
+        alert(data.message);
+        return;
+      }
+      const iconCode = data.weather[0].icon;
+      console.log(data);
+      setWeather({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        description: data.weather[0].description,
+        icon: `https://openweathermap.org/img/wn/${iconCode}@2x.png`,
+      });
+    } catch (error) {
+      console.error("Could not fetch data", error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class="container">
+          <h1>Weather Forecast</h1>
+          <img src={weather.icon} onerror="this.src.display='none'" />
+          <div>
+            <input
+              class="inputField"
+              type="text"
+              placeholder="Enter city/town..."
+              onChange={(e) => setSearch(e.target.value)}
+            ></input>
+            <button class="button" onClick={searchPressed}>
+              Search
+            </button>
+          </div>
+          <div class="descriptionContainer">
+            <p>Location: {weather.location}</p>
+            <p>Condition: {weather.description}</p>
+            <p>Temperature: {weather.temperature} °C</p>
+            <p>Humidity: {weather.humidity}%</p>
+            <p>Wind Speed: {weather.windSpeed} m/s</p>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
